duchimura/GP2040-CE-D_C_Theo

commit ee8ea05eb01578e18ea3e063cef68ef1e6c11ffb

Implement Focus Mode addon (#275)
* Implement Button Lock addon

* Cleanup unnecessary import

www/src/Pages/AddonsConfigPage.jsx
@@ -270,6 +270,13 @@ const schema = yup.object().shape({
 	BootselButtonAddonEnabled:   yup.number().required().label('Boot Select Button Add-On Enabled'),
 	bootselButtonMap:            yup.number().label('BOOTSEL Button Map').validateSelectionWhenValue('BootselButtonAddonEnabled', BUTTON_MASKS),
 
+	FocusModeAddonEnabled:       yup.number().required().label('Focus Mode Add-On Enabled'),
+	focusModePin:                yup.number().label('Focus Mode Pin').validatePinWhenValue('FocusModeAddonEnabled'),
+	focusModeButtonLockEnabled:  yup.number().label('Focus Mode Button Lock Enabled').validatePinWhenValue('FocusModeAddonEnabled'),
+	focusModeOledLockEnabled:    yup.number().label('Focus Mode OLED Lock Enabled').validatePinWhenValue('FocusModeAddonEnabled'),
+	focusModeRgbLockEnabled:     yup.number().label('Focus Mode RGB Lock Enabled').validatePinWhenValue('FocusModeAddonEnabled'),
+	focusModeButtonLockMask:      yup.number().label('Focus Mode Button Lock Map').validateRangeWhenValue('FocusModeAddonEnabled', 0, (1<<20) - 1),
+
 	BuzzerSpeakerAddonEnabled:   yup.number().required().label('Buzzer Speaker Add-On Enabled'),
 	buzzerPin:                   yup.number().label('Buzzer Pin').validatePinWhenValue('BuzzerSpeakerAddonEnabled'),
 	buzzerVolume:                yup.number().label('Buzzer Volume').validateRangeWhenValue('BuzzerSpeakerAddonEnabled', 0, 100),
@@ -390,6 +397,7 @@ const defaultValues = {
 	snesPadDataPin: -1,
 	AnalogInputEnabled: 0,
 	BoardLedAddonEnabled: 0,
+	FocusModeAddonEnabled: 0,
 	BuzzerSpeakerAddonEnabled: 0,
 	BootselButtonAddonEnabled: 0,
 	DualDirectionalInputEnabled: 0,
@@ -480,14 +488,18 @@ const sanitizeData = (values) => {
 			values.analogAdcPinY = parseInt(values.analogAdcPinY);
 		if (!!values.bootselButtonMap)
 			values.bootselButtonMap = parseInt(values.bootselButtonMap);
+		if (!!values.focusModeButtonLockMask)
+			values.focusModeButtonLockMask = parseInt(values.focusModeButtonLockMask);
+		if (!!values.focusModePin)
+			values.focusModePin = parseInt(values.focusModePin);
 		if (!!values.buzzerPin)
 			values.buzzerPin = parseInt(values.buzzerPin);
 		if (!!values.buzzerVolume)
 			values.buzzerVolume = parseInt(values.buzzerVolume);
 		if (!!values.extraButtonMap)
 			values.extraButtonMap = parseInt(values.extraButtonMap);
-		if (!!values.extrabuttonPin)
-			values.extrabuttonPin = parseInt(values.extrabuttonPin);
+		if (!!values.extraButtonPin)
+			values.extraButtonPin = parseInt(values.extraButtonPin);
 		if (!!values.playerNumber)
 			values.playerNumber = parseInt(values.playerNumber);
 		if (!!values.shmupMode)
@@ -629,6 +641,7 @@ export default function AddonsConfigPage() {
 				handleChange,
 				values,
 				errors,
+				setFieldValue
 			}) => (
 				<Form noValidate onSubmit={handleSubmit}>
 					<Section title="Add-Ons Configuration">
@@ -1599,7 +1612,7 @@ export default function AddonsConfigPage() {
 									onChange={handleChange}
 									min={-1}
 									max={29}
-								/>
+								/>	
 								<FormControl type="number"
 									label="Latch Pin"
 									name="snesPadLatchPin"
@@ -1635,6 +1648,92 @@ export default function AddonsConfigPage() {
 							isInvalid={false}
 							checked={Boolean(values.SNESpadAddonEnabled)}
 							onChange={(e) => {handleCheckbox("SNESpadAddonEnabled", values); handleChange(e);}}
+						/>
+					</Section>
+					<Section title="Focus Mode Configuration">
+						<div
+							id="FocusModeAddonOptions"
+							hidden={!values.FocusModeAddonEnabled}>
+							<Row className="mb-3">
+								<FormControl type="number"
+									label="Focus Mode Pin"
+									name="focusModePin"
+									className="form-select-sm col-3"
+									groupClassName="col-sm-3 mb-3"
+									value={values.focusModePin}
+									error={errors.focusModePin}
+									isInvalid={errors.focusModePin}
+									onChange={handleChange}
+									min={-1}
+									max={29} />
+								<div className="col-sm-3">
+									<FormCheck
+										label="Lock OLED Screen"
+										className="form-check-sm"
+										type="switch"
+										reverse
+										id="FocusModeAddonOLEDButton"
+										isInvalid={false}
+										checked={Boolean(values.focusModeOledLockEnabled)}
+										onChange={(e) => { handleCheckbox("focusModeOledLockEnabled", values); handleChange(e); }} />
+								</div>
+								<div className="col-sm-3">
+									<FormCheck
+										label="Lock RGB LED"
+										className="form-check-sm"
+										type="switch"
+										reverse
+										id="FocusModeAddonButton"
+										isInvalid={false}
+										checked={Boolean(values.focusModeRgbLockEnabled)}
+										onChange={(e) => { handleCheckbox("focusModeRgbLockEnabled", values); handleChange(e); }} />
+								</div>
+								<div className="col-sm-3">
+									<FormCheck
+										label="Lock Buttons"
+										className="form-check-sm"
+										type="switch"
+										reverse
+										id="FocusModeAddonButton"
+										isInvalid={false}
+										checked={Boolean(values.focusModeButtonLockEnabled)}
+										onChange={(e) => { handleCheckbox("focusModeButtonLockEnabled", values); handleChange(e); }}
+									/>
+								</div>
+								<Row>
+									{BUTTON_MASKS.map(mask => (values.focusModeButtonLockMask & mask.value) ? <FormSelect
+										name="focusModeButtonLockMask"
+										className="form-select-sm"
+										groupClassName="col-sm-3 mb-3"
+										value={values.focusModeButtonLockMask & mask.value}
+										error={errors.focusModeButtonLockMask}
+										isInvalid={errors.focusModeButtonLockMask}
+										onChange={(e) => { setFieldValue("focusModeButtonLockMask", values.focusModeButtonLockMask ^ mask.value | e.target.value); }}
+									>
+										{BUTTON_MASKS.map((o, i) => <option key={`focusModeButtonLockMask-option-${i}`} value={o.value}>{o.label}</option>)}
+									</FormSelect> : <></>)}
+									<FormSelect
+										name="focusModeButtonLockMask"
+										className="form-select-sm"
+										groupClassName="col-sm-3 mb-3"
+										value={0}
+										error={errors.focusModeButtonLockMask}
+										isInvalid={errors.focusModeButtonLockMask}
+										onChange={(e) => { setFieldValue("focusModeButtonLockMask", values.focusModeButtonLockMask | e.target.value); }}
+									>
+										{BUTTON_MASKS.map((o, i) => <option key={`focusModeButtonLockMask-option-${i}`} value={o.value}>{o.label}</option>)}
+									</FormSelect>
+								</Row>
+							</Row>
+						</div>
+						<FormCheck
+							label="Enabled"
+							type="switch"
+							id="FocusModeAddonButton"
+							reverse
+							isInvalid={false}
+							checked={Boolean(values.FocusModeAddonEnabled)}
+							onChange={(e) => { handleCheckbox("FocusModeAddonEnabled", values); handleChange(e);}}
 						/>
 					</Section>
 					<div className="mt-3">

CMakeLists.txt
@@ -123,6 +123,7 @@ src/configs/webconfig.cpp
 src/addons/analog.cpp
 src/addons/board_led.cpp
 src/addons/bootsel_button.cpp
+src/addons/focus_mode.cpp
 src/addons/buzzerspeaker.cpp
 src/addons/dualdirectional.cpp
 src/addons/extra_button.cpp

configs/Pico/BoardConfig.h
@@ -240,6 +240,14 @@
                             // For directions, use GAMEPAD_MASK_DU, GAMEPAD_MASK_DD, GAMEPAD_MASK_DL and GAMEPAD_MASK_DR
 #define EXTRA_BUTTON_PIN -1
 
+// Button Lock Add-on setting
+#define FOCUS_MODE_BUTTON_MASK 0 // 0 means none, get other mask from GamepadState.h
+                            // For directions, use GAMEPAD_MASK_DU, GAMEPAD_MASK_DD, GAMEPAD_MASK_DL and GAMEPAD_MASK_DR
+#define FOCUS_MODE_PIN -1
+#define FOCUS_MODE_OLED_LOCK_ENABLED 0
+#define FOCUS_MODE_RGB_LOCK_ENABLED 0
+#define FOCUS_MODE_BUTTON_LOCK_ENABLED 0
+
 // Keyboard Mapping Configuration
 // List of HID keycodes can be located here: https://github.com/hathach/tinyusb/blob/3623ba1884ddff23e9b64766cb6dd032f1425846/src/class/hid/hid.h#L356
 // Even for the modifier keys, HID_KEY entries should be used as the implementation expects those and will convert as necessary.

headers/addons/focus_mode.h
@@ -1,0 +1,46 @@
+#ifndef _FocusMode_H
+#define _FocusMode_H
+
+#include "gpaddon.h"
+#include "gamepad.h"
+
+#ifndef FOCUS_MODE_ENABLED
+#define FOCUS_MODE_ENABLED 0
+#endif
+
+#ifndef FOCUS_MODE_PIN
+#define FOCUS_MODE_PIN -1
+#endif
+
+#ifndef FOCUS_MODE_BUTTON_MASK
+#define FOCUS_MODE_BUTTON_MASK 0
+#endif
+
+#ifndef FOCUS_MODE_OLED_LOCK_ENABLED
+#define FOCUS_MODE_OLED_LOCK_ENABLED 0
+#endif
+
+#ifndef FOCUS_MODE_RGB_LOCK_ENABLED
+#define FOCUS_MODE_RGB_LOCK_ENABLED 0
+#endif
+
+#ifndef FOCUS_MODE_BUTTON_LOCK_ENABLED
+#define FOCUS_MODE_BUTTON_LOCK_ENABLED 0
+#endif
+
+// FocusMode Module Name
+#define FocusModeName "FocusMode"
+
+class FocusModeAddon : public GPAddon {
+public:
+	virtual bool available();
+	virtual void setup();       // FocusMode Setup
+	virtual void process();     // FocusMode Process
+	virtual void preprocess() {}
+	virtual std::string name() { return FocusModeName; }
+private:
+	uint32_t buttonLockMask;
+	uint8_t focusModePin;
+};
+
+#endif  // _FocusMode_H_

headers/addons/i2cdisplay.h
@@ -209,6 +209,8 @@ private:
 	DisplayMode getDisplayMode();
 	DisplayMode prevDisplayMode;
 	uint16_t prevButtonState;
+	bool isFocusModeEnabled;
+	bool focusModePrevState;
 };
 
 #endif

headers/addons/neopicoleds.h
@@ -190,6 +190,8 @@ private:
 	NeoPicoPlayerLEDs * neoPLEDs = nullptr;
 	AnimationStation as;
 	std::map<std::string, int> buttonPositions;
+	bool isFocusModeEnabled;
+	bool focusModePrevState;
 };
 
 #endif

lib/AnimationStation/src/AnimationStation.cpp
@@ -191,3 +191,7 @@ void AnimationStation::IncreaseBrightness() {
   else if (AnimationStation::options.brightness > getBrightnessStepSize())
     AnimationStation::SetBrightness(brightnessSteps);
 }
+
+void AnimationStation::DimBrightnessTo0() {
+  AnimationStation::brightnessX = 0;
+}

lib/AnimationStation/src/AnimationStation.hpp
@@ -113,6 +113,7 @@ public:
   static void SetBrightness(uint8_t brightness);
   static void DecreaseBrightness();
   static void IncreaseBrightness();
+  static void DimBrightnessTo0();
   static void SetOptions(AnimationOptions options);
 
   Animation* baseAnimation;

commit c8b4f0fbc9f192b9578f462f595557a2d42741ae

Fix regression on addon settings saving (#359)
This was introduced in ee8ea05.

Settings were not correctly validated since the pin validator is used to validate boolean values, which caused it to always fail silently.

www/src/Pages/AddonsConfigPage.jsx
@@ -147,11 +147,11 @@ const verifyAndSavePS4 = async () => {
 				}
 				const hashed = CryptoJS.SHA256(bytes);
 				const signNonce = key.sign(hashed, CryptoJS.SHA256, "sha256");
-				
+
 				if (signNonce === false) {
 					throw new Error("Bad Private Key");
 				}
-				
+
 				// Private key worked!
 				var BigInteger = require('jsbn').BigInteger;
 
@@ -269,15 +269,15 @@ const schema = yup.object().shape({
 
 	BoardLedAddonEnabled:        yup.number().required().label('Board LED Add-On Enabled'),
 	onBoardLedMode:              yup.number().label('On-Board LED Mode').validateSelectionWhenValue('BoardLedAddonEnabled', ON_BOARD_LED_MODES),
- 
+
 	BootselButtonAddonEnabled:   yup.number().required().label('Boot Select Button Add-On Enabled'),
 	bootselButtonMap:            yup.number().label('BOOTSEL Button Map').validateSelectionWhenValue('BootselButtonAddonEnabled', BUTTON_MASKS),
 
 	FocusModeAddonEnabled:       yup.number().required().label('Focus Mode Add-On Enabled'),
 	focusModePin:                yup.number().label('Focus Mode Pin').validatePinWhenValue('FocusModeAddonEnabled'),
-	focusModeButtonLockEnabled:  yup.number().label('Focus Mode Button Lock Enabled').validatePinWhenValue('FocusModeAddonEnabled'),
-	focusModeOledLockEnabled:    yup.number().label('Focus Mode OLED Lock Enabled').validatePinWhenValue('FocusModeAddonEnabled'),
-	focusModeRgbLockEnabled:     yup.number().label('Focus Mode RGB Lock Enabled').validatePinWhenValue('FocusModeAddonEnabled'),
+	focusModeButtonLockEnabled:  yup.number().label('Focus Mode Button Lock Enabled').validateRangeWhenValue('FocusModeAddonEnabled', 0, 1),
+	focusModeOledLockEnabled:    yup.number().label('Focus Mode OLED Lock Enabled').validateRangeWhenValue('FocusModeAddonEnabled', 0, 1),
+	focusModeRgbLockEnabled:     yup.number().label('Focus Mode RGB Lock Enabled').validateRangeWhenValue('FocusModeAddonEnabled', 0, 1),
 	focusModeButtonLockMask:      yup.number().label('Focus Mode Button Lock Map').validateRangeWhenValue('FocusModeAddonEnabled', 0, (1<<20) - 1),
 
 	BuzzerSpeakerAddonEnabled:   yup.number().required().label('Buzzer Speaker Add-On Enabled'),
@@ -330,7 +330,7 @@ const schema = yup.object().shape({
 	pinShmupDial:                yup.number().label('Shmup Dial Pin').validatePinWhenValue('TurboInputEnabled'),
 	turboShotCount:              yup.number().label('Turbo Shot Count').validateRangeWhenValue('TurboInputEnabled', 5, 30),
 	shmupMode:                   yup.number().label('Shmup Mode Enabled').validateRangeWhenValue('TurboInputEnabled', 0, 1),
-	shmupMixMode:                yup.number().label('Shmup Mix Priority').validateSelectionWhenValue('TurboInputEnabled', DUAL_STICK_MODES), 
+	shmupMixMode:                yup.number().label('Shmup Mix Priority').validateSelectionWhenValue('TurboInputEnabled', DUAL_STICK_MODES),
 	shmupAlwaysOn1:              yup.number().label('Turbo-Button 1 (Always On)').validateSelectionWhenValue('TurboInputEnabled', BUTTON_MASKS),
 	shmupAlwaysOn2:              yup.number().label('Turbo-Button 2 (Always On)').validateSelectionWhenValue('TurboInputEnabled', BUTTON_MASKS),
 	shmupAlwaysOn3:              yup.number().label('Turbo-Button 3 (Always On)').validateSelectionWhenValue('TurboInputEnabled', BUTTON_MASKS),
@@ -609,15 +609,15 @@ const sanitizeData = (values) => {
 
 function flattenObject(object) {
 	var toReturn = {};
-  
+
 	for (var i in object) {
 	  if (!object.hasOwnProperty(i)) continue;
-  
+
 	  if (typeof object[i] == "object" && object[i] !== null) {
 		var flatObject = flattenObject(object[i]);
 		for (var x in flatObject) {
 		  if (!flatObject.hasOwnProperty(x)) continue;
-  
+
 		  toReturn[i + "." + x] = flatObject[x];
 		}
 	  } else {
@@ -666,7 +666,7 @@ export default function AddonsConfigPage() {
 	const onSuccess = async (values) => {
 		const flattened = flattenObject(storedData)
 		const valuesCopy = schema.cast((values)) // Strip invalid values
-		
+
 		// Compare what's changed and set it to resultObject
 		let resultObject = {}
 		Object.entries(flattened)?.map(entry => {
@@ -1696,7 +1696,7 @@ export default function AddonsConfigPage() {
 									onChange={handleChange}
 									min={-1}
 									max={29}
-								/>	
+								/>
 								<FormControl type="number"
 									label="Latch Pin"
 									name="snesPadLatchPin"